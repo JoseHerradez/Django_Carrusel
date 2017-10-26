4d475c4a166e69eea64cbe30ce60146bce719f3b
nek27 <12-11469@usb.ve>
2017-10-26 18:23:25 -0400

    Pruebas unitarias y paginator como clase
    Subo las pruebas unitarias en tests.py y la implementacion que estaba hecha del paginator escrita como una clase en paginator .py, modifique el views para que use los metodos en pagintor.py en vez de tener el codigo alli

--- carrusel/paginacion/tests.py
+++ carrusel/paginacion/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from paginacion.paginator import *
 
-# Create your tests here.
+class PaginationTestCase(TestCase):
+	def setUp(self):
+		self.paginator = None
+		
+	def test_crearPaginador(self):
+		lista = ['asd', 'bobba', 'foo', 'bar' , 'dsa', 'sad', 'ninesoft']
+		
+		#Caso trivial
+		self.paginator = TUIsDPaginator(lista, 2)
+		self.assertNotEqual(self.paginator, None)
+		self.paginator = None
+		
+		#Caso borde
+		self.paginator = TUIsDPaginator([], 2)
+		self.assertNotEqual(self.paginator, None)
+		self.paginator = None
+		
+		#Casos en que deberia fallar
+		with self.assertRaises(ValueError):
+			self.paginator = TUIsDPaginator([], -1)
+		self.paginator = None
+		
+		with self.assertRaises(ValueError):
+			self.paginator = TUIsDPaginator([], 0)
+		self.paginator = None
+		
+	def test_seleccionarPagina(self):
+		lista = ['asd', 'bobba', 'foo', 'bar' , 'dsa', 'sad', 'ninesoft']
+		self.paginator = TUIsDPaginator(lista, 3)
+		
+		#Caso trivial
+		elems = self.paginator.get_elems_from_page(1).object_list
+		self.assertEquals(elems, ['asd', 'bobba', 'foo'])
+		
+		#Casos borde
+		elems = self.paginator.get_elems_from_page(3).object_list
+		self.assertEquals(elems, ['ninesoft'])
+		
+		#Casos en que deberia fallar
+		elems = self.paginator.get_elems_from_page(-1).object_list
+		self.assertEquals(elems, ['ninesoft'])
+		
+		elems = self.paginator.get_elems_from_page(5).object_list
+		self.assertEquals(elems, ['ninesoft'])
+		
+		elems = self.paginator.get_elems_from_page(2.9).object_list
+		self.assertEquals(elems, ['asd', 'bobba', 'foo'])
--- carrusel/paginacion/views.py
+++ carrusel/paginacion/views.py
@@ -1,21 +1,18 @@
 from django.shortcuts import render
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from paginacion.paginator import *
 
 def index(request):
     # for i in range(0, 30):
     #     new_user = User(username=str(i), first_name=str(i), email=str(i) + '@gmail.com')
     #     new_user.save()
     contact_list = ['John', 'Paul', 'George', 'Ringo']
-    paginator = Paginator(contact_list, 2) # Show 2 contacts per page
-
+    #paginator = Paginator(contact_list, 2) # Show 2 contacts per page
+    paginator2 = TUIsDPaginator(contact_list, 2)
+    
     page = request.GET.get('page', 1)
-    try:
-        contacts = paginator.page(page)
-    except PageNotAnInteger:
-        # If page is not an integer, deliver first page.
-        contacts = paginator.page(1)
-    except EmptyPage:
-        # If page is out of range (e.g. 9999), deliver last page of results.
-        contacts = paginator.page(paginator.num_pages)
-
+    contacts = paginator2.get_elems_from_page(page)
+	
     return render(request, 'list.html', {'contacts': contacts})
+   
+
+	
