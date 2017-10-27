e4b8f31d090125ba0daa08dd2fcdee942bbaa0e5
Jose Herradez Hernandez <Jose Herradez Hernandez>
2017-10-27 08:21:43 -0400

    Bug fixed

--- carrusel/paginacion/models.py
+++ carrusel/paginacion/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class List(models.Model):
-	ident = models.IntegerField()
-	
+    ident = models.IntegerField()
+
 class ListElem(models.Model):
-	ident = models.ForeignKey(List)
-	elem = models.CharField(max_length = 200)
-	
-	def __str__(self):
-		return str(self.elem)
+    ident = models.ForeignKey(List)
+    elem = models.CharField(max_length=200)
+
+    def __str__(self):
+        return str(self.elem)
